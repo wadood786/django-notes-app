4a36c756d899ca67a49e14e1c0032a0b8b9b6a47
AbdulWadood <71840338+wadood786@users.noreply.github.com>
2025-09-26 18:54:45 +0500

    Update urls.py

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.urls import path
+from .views import github_webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
